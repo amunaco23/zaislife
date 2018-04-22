8cb9734a34640d2d07a7d1d1ea9128377c337d6a
alexjmartin65 <alexander.martin45@gmail.com>
2018-04-22 16:24:03 -0500

    Fix desktop style issues, add about, top filters, add loading gif

--- web-2/js/main.js
+++ web-2/js/main.js
@@ -20,7 +20,24 @@ function initTableData(){
 function showInfo(data, tabletop) {
     // console.log(data);
     tableData = data.MASTER.elements;
-    showTableDataByFilter(defaultCity);
+    setFilters();
+    $(".loader").hide();
+    $(".table-row-wrapper").show();
+}
+
+function setFilters(){
+    //_.uniq([1, 2, 1, 4, 1, 3]);
+    var cities = _.pluck(tableData, 'city');
+    cities = _.uniq(cities);
+    cities = _.filter(cities, function(city){ return city && city != ""; });
+    var handleBarData = {items: cities};
+    var source   = document.getElementById("city-row").innerHTML;
+    var template = Handlebars.compile(source);
+    var html = template(handleBarData);
+    $(".city-container").html(html);
+    var first = $(".filter-item").first();
+    filterByItem(first);
+    $(".filter-row .filter-item").click(filterRows);    
 }
 
 function filterRows(){
@@ -30,10 +47,14 @@ function filterRows(){
     }
 
     $(".selected").removeClass("selected");
-    filter.addClass("selected");
-    var filterText = filter.text().trim();
-    showTableDataByFilter(filterText);
+    filterByItem(filter);
 }
+
+function filterByItem(filterRow){
+    filterRow.addClass("selected");
+    var filterText = filterRow.text().trim();
+    showTableDataByFilter(filterText);
+};
 
 function showTableDataByFilter(city){
     var filterdData = _.filter(tableData, function(item){ return item.city.toUpperCase() == city.toUpperCase(); });
